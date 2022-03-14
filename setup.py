85947f05f08c3e3570c0dd227f9c3fdf84078e5b
Thraax <inthaak@gmail.com>
2022-03-14 17:42:28 +0200

    Adding project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict the auction sale price for a piece of heavy equipment to create a "blue book" for bulldozers.',
+    author='Yusuf Taha',
+    license='',
+)
